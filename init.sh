7c20163c96ce4617c8811fbe7b0ebd5372efb300
Konda Srikanth <konda2srikanth@gmail.com>
2024-11-21 18:56:54 +0530

    Auto Push

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
-terraform init -backend-config=env-${env}/state.tfvars
-terraform plan -var-file=env-${env}/main.tfvars -var vault_token=${vault_token} -var ssh_pwd=${ssh_pwd}
-terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars -var vault_token=${vault_token} -var ssh_pwd=${ssh_pwd}
+# terraform init -backend-config=env-${env}/state.tfvars
+# terraform plan -var-file=env-${env}/main.tfvars -var vault_token=${vault_token} -var ssh_pwd=${ssh_pwd}
+# terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars -var vault_token=${vault_token} -var ssh_pwd=${ssh_pwd}
+
+# #rm -rf .terraform ; git pull ; terraform init -backend-config=env-dev/state.tfvars ; terraform plan -var-file=env-dev/main.tfvars  ; terraform apply -auto-approve -var-file=env-dev/main.tfvars 
